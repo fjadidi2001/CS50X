af5133be39417e6f712a0e9998884d46db7fe27a
fjadidi2001 <jadidi7300@gmail.com>
2023-08-12 15:48:30 -0400

    added const.c 1

---
+++ C-learning/Variable/02-constants.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//A constant stores a value that cannot be changed from its initial assignment. 
+
+//use uppercase 
+int main()
+{
+    const double PI = 3.14;
+    printf("%f", PI);
+    return 0;
+
+        
+}

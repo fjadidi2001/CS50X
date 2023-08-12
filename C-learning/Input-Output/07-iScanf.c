e04bf8c49af16f2b42cc1dafec75d6593491819a
fjadidi2001 <jadidi7300@gmail.com>
2023-08-12 17:26:56 -0400

    add imp comment

--- C-learning/Input-Output/07-iScanf.c
+++ C-learning/Input-Output/07-iScanf.c
@@ -5,6 +5,7 @@ int main() {
     float num;
     char text[20];
     scanf("%d %f %s", &x, &num, text);
+    //The & isn't needed for a string because a string name acts as a pointer.
     printf("%d %f %s", x, num, text);
     
 }

de5bfd9f29df2af89dd7608b759bbd9558dd8cf5
fjadidi2001 <jadidi7300@gmail.com>
2023-08-12 17:13:47 -0400

    added 06o data

---
+++ C-learning/Input-Output/06-oPuts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    char f[100];
+    gets(f);
+    printf("entered:");
+    puts(f);
+    //The puts() function is used to display output as a string.
+
+
+    return 0;
+
+}

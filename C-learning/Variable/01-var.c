35b188bd630a7e5b6aac4b9ccdb05e8ce164c0b5
fjadidi2001 <jadidi7300@gmail.com>
2023-08-12 15:37:17 -0400

    add new declare

---
+++ C-learning/Variable/01-var.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// A variable is a name for an area in memory.
+
+int main()
+{
+    int a, b;
+    float salary = 56.1245;
+    char letter = 'f';
+    a = 8;
+    b = 34;
+    int c = a*b;
+    printf("%d \n%f \n%c \n", c, salary, letter);
+    
+    return 0;
+
+
+}

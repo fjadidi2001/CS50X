ecd09d059139a92d42daaac29945a70ff870b0f9
fjadidi2001 <jadidi7300@gmail.com>
2023-08-13 13:28:42 -0400

    fix bugs

--- C-learning/Operators/03-typeConversion.c
+++ C-learning/Operators/03-typeConversion.c
@@ -4,7 +4,9 @@ int main()
     int num1;
     float num2;
     float result;
-    scanf("%d %f", num1, num2);
-    printf("The result is %8.5f", result);
+    scanf("%d %f", &num1, &num2);
+    result = num1 * num2;
+    printf("The result is %.5f", result);
+    return 0;
 
 }

e987c305ba6c1f76eee714f75ef294289e50a1df
fjadidi2001 <jadidi7300@gmail.com>
2023-08-15 11:38:23 -0400

    update 02-fElse

---
+++ C-learning/Conditionals/02-ifElse.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+
+    char f[250];
+    scanf("%s", f);
+    if (f == "fateme")
+    {
+        printf("f is fateme");
+    }
+    else
+    {
+        printf("f is another thing");
+    }
+}  

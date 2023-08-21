1d10e8ca9db8f475cb2c970d4eebaabd2549adbd
fjadidi2001 <jadidi7300@gmail.com>
2023-08-20 22:08:18 -0400

    added data to 02looparray

---
+++ C-learning/Array/02-loopArray.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    float purchases[3] = {10.99, 14.25, 90.50};
+    float total = 0;
+    int k;
+
+/* total the purchases */
+    for (k = 0; k < 3; k++) {
+    total += purchases[k];
+}
+   
+printf("Purchases total is %6.2f\n", total);
+/* Output:  Purchases total is 115.74 */
+}

78c07b43ec76609026f3f005e2acf42787a8e6d1
fjadidi2001 <jadidi7300@gmail.com>
2023-08-20 21:41:21 -0400

    update 01-array

--- C-learning/Array/01-array.c
+++ C-learning/Array/01-array.c
@@ -5,5 +5,8 @@ int main()
     float array2[5] = {3.2, 6.25, 7.1, 8.3, 1.0258};
     int array3[5] = {3, 5,1}; //Missing values are set to 0.
     printf("The second element is %4.2f\n", array2[4]);
+    array2[1] = 4.125;
+    printf("The element change to new number %d\n", array2[1]);
+    
 
 }

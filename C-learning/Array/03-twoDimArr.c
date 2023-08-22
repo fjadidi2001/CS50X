78a093bbdaa3cdc84fd878df4dcc9ce5eb5e167f
fjadidi2001 <jadidi7300@gmail.com>
2023-08-21 23:35:39 -0400

    fix 03 array problem

--- C-learning/Array/03-twoDimArr.c
+++ C-learning/Array/03-twoDimArr.c
@@ -9,7 +9,7 @@ int main()
         {5 ,15}
     };
     int c[2][3] = { {3, 2, 6}, {4, 5, 20} }; 
-    printf("Element 2 in row 4 is %d\n", b[4][2]);
+    printf("Element 2 in row 4 is %d\n", b[3][1]);
 
     printf("Element 3 in row 2 is %d\n", c[1][2]); /* 20 */
     c[1][2] = 25;

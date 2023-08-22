4603f98bae041a6f252bdc6914e1a1ebc5e1e1ba
fjadidi2001 <jadidi7300@gmail.com>
2023-08-21 23:03:51 -0400

    added data to 03array

--- C-learning/Array/03-twoDimArr.c
+++ C-learning/Array/03-twoDimArr.c
@@ -2,5 +2,11 @@
 int main()
 {
     int a[2][3];
-    printf("%d is a dimensional array.", a);
+    int b[4][2] = {
+        {3, 1},
+        {1, 8},
+        {2, 4},
+        {5 ,15}
+    };
+    printf("%d is a dimensional array.", b);
 }

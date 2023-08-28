16bc718b81b5b99b6c4882107a245b8820e1a7b6
fjadidi2001 <jadidi7300@gmail.com>
2023-08-28 11:44:15 -0400

    fix error & debugging that

--- C-learning/DataType/string/01-string.c
+++ C-learning/DataType/string/01-string.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(){
-    int str1[6] = "hello";
-    int str2[ ] = "world";
+    char str1[6] = "hello";
+    char str2[ ] = "world";
     printf("str1: %s, str2: %s", str1, str2);
+    return 0;
 }

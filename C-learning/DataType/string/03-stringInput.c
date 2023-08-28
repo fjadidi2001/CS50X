519fde42b459d0179a1a1ddae4151051603e0869
fjadidi2001 <jadidi7300@gmail.com>
2023-08-28 12:12:33 -0400

    added comment to 03string.c

--- C-learning/DataType/string/03-stringInput.c
+++ C-learning/DataType/string/03-stringInput.c
@@ -5,7 +5,9 @@ int main() {
     int age;
     printf("Enter your first name and age: \n");
     scanf("%s %d", first_name, &age);
-    
+    /*
+    there is no need for & to access the variable address because an array name acts as a pointer.
+    */
     printf("\nHi, %s. Your age is %d", first_name, age);
     
     return 0;

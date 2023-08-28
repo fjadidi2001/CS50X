3bae2e03bcf4a3c5df6e94910e7f40f65de628f5
fjadidi2001 <jadidi7300@gmail.com>
2023-08-28 13:50:33 -0400

    update and fix 07string.c

--- C-learning/DataType/string/07-stringH.c
+++ C-learning/DataType/string/07-stringH.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <string.h>
+
 int main()
 {
-    char str1[ ] = "FAtemE Must Be die!!";
-    char str2[ ] = "But Not TodaY!";
-    strcat(str1,str2);
-    printf("len str1: %d", strlen(str1));
-    strcpy(str1,str2);
-    printf("str1 is now %s \n", str1);
+    char s1[ ] = "The grey fox";
+    char s2[ ] = " jumped.";
+    
+    strcat(s1, s2);
+    printf("%s\n", s1);
+    printf("Length of s1 is %d\n", strlen(s1));
+    strcpy(s1, s2);
+    printf("s1 is now %s \n", s1);
+
     return 0;
-
-
-
-}
+} 

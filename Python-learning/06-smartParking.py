a560ae1a0e8c3f1742078d436d003be6f26636ad
fjadidi2001 <jadidi7300@gmail.com>
2023-09-05 10:34:23 -0400

    added data to 06.py

---
+++ Python-learning/06-smartParking.py
@@ -0,0 +1,12 @@
+# Take the number of available spaces as an input
+spaces = int(input())
+
+# Display message if spaces are available
+if spaces > 0:
+    print("Available spaces")
+else:
+
+
+# Display a different message if spaces are not available
+
+    print("Sorry, the parking lot is full")

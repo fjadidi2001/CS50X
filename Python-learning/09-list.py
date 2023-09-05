3444124de7087081307bd4f86dd6647d134a3da2
fjadidi2001 <jadidi7300@gmail.com>
2023-09-05 12:23:00 -0400

    added data to 09.py

---
+++ Python-learning/09-list.py
@@ -0,0 +1,11 @@
+employe = ["Aftabi",
+            "Sajad",
+            "Hossein",
+            "Atashi",
+            "zi"]
+
+print(employe)
+print(employe[0])
+print(employe[4])
+for i in range(employe):
+    print(i)

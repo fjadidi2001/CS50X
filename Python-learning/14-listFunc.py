104732be728b3a5c4fc6f2f7803d42d26088d607
fjadidi2001 <jadidi7300@gmail.com>
2023-09-06 16:37:03 -0400

    update 14.py

--- Python-learning/14-listFunc.py
+++ Python-learning/14-listFunc.py
@@ -4,3 +4,9 @@ print(len(movies))
 songs = ["Yesterday", "Hello", "Believer"]
 songs.append("Imagine")
 print(songs)
+
+
+items = ["book", "pen", "pencil"]
+items.insert(2,"marker")
+print(items)
+print(items[2])

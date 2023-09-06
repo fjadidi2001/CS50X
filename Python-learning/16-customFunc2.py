8b395fba5a1ac1134f3af0f26051348a6b4ea04d
fjadidi2001 <jadidi7300@gmail.com>
2023-09-06 17:25:26 -0400

    update 16.py

--- Python-learning/16-customFunc2.py
+++ Python-learning/16-customFunc2.py
@@ -27,13 +27,3 @@ def shipping_cost(weight):
 
 # function call
 shipping_cost(weight)
-
-
-# ==================================================
-def rect(length, width):
-  area = length * width
-  perimeter = 2 * length + 2 * width
-  return area, perimeter #2 values
-
-x, y = rect(50, 100) #2 variables
-print(x, y)

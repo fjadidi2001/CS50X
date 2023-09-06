ccd7df1b0432613f77a4f00ef73ae18adcc3076e
fjadidi2001 <jadidi7300@gmail.com>
2023-09-06 17:22:21 -0400

    update 16.py

--- Python-learning/16-customFunc2.py
+++ Python-learning/16-customFunc2.py
@@ -1,27 +1,39 @@
-#Defining function
+# Defining function
 def bmi(weight, height):
   index = weight / (height * height)
   return index  #sends the return value back
 
-#Calling function and using return value
+# Calling function and using return value
 patient_5 = bmi(61, 1.83) #stores return value
 print("underweight:", patient_5 < 18.5)
 
-#Another call
+# Another call
 patient_7 = bmi(75, 1.74)
 print("underweight:", patient_7 < 18.5)
 
 
+# ====================================================
 
-#taking the weight as input
+
+# taking the weight as input
 weight = int(input())
 
-#complete the function
+# complete the function
 def shipping_cost(weight):
     index = weight * 5
     print(index)
 
      
 
-#function call
+# function call
 shipping_cost(weight)
+
+
+# ==================================================
+def rect(length, width):
+  area = length * width
+  perimeter = 2 * length + 2 * width
+  return area, perimeter #2 values
+
+x, y = rect(50, 100) #2 variables
+print(x, y)

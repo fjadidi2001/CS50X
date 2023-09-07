1417b15dc9581b193a4ade84edbcd14dc47922ca
fjadidi2001 <jadidi7300@gmail.com>
2023-09-07 02:18:59 -0400

    update 21.py

--- Python-learning/21-dicGet.py
+++ Python-learning/21-dicGet.py
@@ -7,3 +7,23 @@ pairs = {1: "apple",
 print(pairs.get("orange"))
 print(pairs.get(7, 42))
 print(pairs.get(12345, "not found"))
+
+
+
+
+# -----------------------------------
+data = {
+    'Singapore': 1,
+    'Ireland': 6,
+    'United Kingdom': 7,
+    'Germany': 27,
+    'Armenia': 34,
+    'United States': 17,
+    'Canada': 9,
+    'Italy': 74
+}
+
+
+country = str(input())
+
+print(data.get(country, "Not found")) 

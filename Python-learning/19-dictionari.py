2452721b0f60e956fdae38351d0ba3f84135da72
fjadidi2001 <jadidi7300@gmail.com>
2023-09-07 01:51:16 -0400

    create 19.py

---
+++ Python-learning/19-dictionari.py
@@ -0,0 +1,17 @@
+car = {
+    'brand':'BMW',
+    'year': 2018,
+    'color': 'red',
+    'mileage': 15000
+}
+sus = input()
+if sus == "brand":
+    print(car['brand'])
+elif sus == "year":
+    print(car['year'])
+elif sus == "color":
+    print(car['color'])
+elif sus == "mileage":
+    print(car['mileage'])
+else :
+    print("be hummbele")

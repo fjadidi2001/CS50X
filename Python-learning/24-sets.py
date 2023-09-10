4a48e8ea1563e98ee703b745e825b727aa727a07
fjadidi2001 <jadidi7300@gmail.com>
2023-09-10 16:30:42 -0400

    update 24.py

--- Python-learning/24-sets.py
+++ Python-learning/24-sets.py
@@ -28,3 +28,7 @@ print(first & second)
 print(first - second)
 print(second - first)
 print(first ^ second)
+
+skills = {'Python', 'HTML', 'SQL', 'C++', 'Java', 'Scala'}
+job_skills = {'HTML', 'CSS', 'JS', 'C#', 'NodeJS'}
+print(*(skills & job_skills))

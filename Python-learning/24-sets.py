bed3a64e51e84efaffb69c8e1b79159e46064414
fjadidi2001 <jadidi7300@gmail.com>
2023-09-10 16:22:29 -0400

    update 24.py

--- Python-learning/24-sets.py
+++ Python-learning/24-sets.py
@@ -4,3 +4,16 @@ print(3 in num_set)
 
 # Sets cannot contain duplicate elements.
 
+letters = {"a", "b", "c", "d"}
+if "e" not in letters:
+  print(1)
+else: 
+  print(2)
+
+
+
+nums = {1, 2, 1, 3, 1, 4, 5, 6}
+print(nums)
+nums.add(-7)
+nums.remove(3)
+print(nums)

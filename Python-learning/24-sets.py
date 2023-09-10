8455b58da15b4f8d1bc5182b45f214984e64b969
fjadidi2001 <jadidi7300@gmail.com>
2023-09-10 16:24:40 -0400

    update 24.py

--- Python-learning/24-sets.py
+++ Python-learning/24-sets.py
@@ -20,3 +20,11 @@ print(nums)
 
 # The len() function can be used to return the number of elements of a set.
 
+first = {1, 2, 3, 4, 5, 6}
+second = {4, 5, 6, 7, 8, 9}
+
+print(first | second)
+print(first & second)
+print(first - second)
+print(second - first)
+print(first ^ second)

b73f68ebfa0a8c1258e987ec7743606f069fb846
fjadidi2001 <jadidi7300@gmail.com>
2023-09-16 18:35:16 -0400

    update 29.py

--- Python-learning/29-map.py
+++ Python-learning/29-map.py
@@ -10,3 +10,9 @@ nums = [11, 22, 33, 44, 55]
 
 result2 = list(map(lambda x: x+5, nums))
 print(result2)
+
+
+salaries = [2000, 1800, 3100, 4400, 1500]
+bonus = int(input())
+salaries=(list(map(lambda x : x+bonus,salaries )))
+print(salaries)

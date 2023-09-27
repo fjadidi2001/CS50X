dac17baa97d50b7a3d8ca946fb92f8f9fc32c076
fjadidi2001 <jadidi7300@gmail.com>
2023-09-27 16:46:42 -0400

    update 10.c

---
+++ C-learning/DataType/string/10-textConvertdecimal.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char text[] = "12345"; // Replace this with your input text
+    int decimalValue = atoi(text);
+    
+    printf("Text: %s\n", text);
+    printf("Decimal Value: %d\n", decimalValue);
+    
+    return 0;
+}

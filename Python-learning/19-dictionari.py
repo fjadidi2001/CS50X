c43caf174c914af905fab98c76a25f81daf9054c
fjadidi2001 <jadidi7300@gmail.com>
2023-10-03 17:06:00 -0400

    update 19.py

--- Python-learning/19-dictionari.py
+++ Python-learning/19-dictionari.py
@@ -15,3 +15,15 @@ elif sus == "mileage":
     print(car['mileage'])
 else :
     print("be hummbele")
+
+
+# other way
+
+car = {
+    'brand':'BMW',
+    'year': 2018,
+    'color': 'red',
+    'mileage': 15000
+}
+choice = input()
+print(car[choice])
